34e039024e0c87dd37e615b58efb7d5809ee9da8
D0KD0 <113081917+D0KD0@users.noreply.github.com>
2022-10-09 19:57:51 -0700

    final draft

--- README.md
+++ README.md
@@ -1,32 +1,13 @@
 # can_u_answer_this
 
-GIVEN I am taking a code quiz
-WHEN I click the start button
-시작 버튼 누르면
+## Description
+In this project, I made a quiz about South Korea by using JavaScript.
+Start code was not provided and I started from scratch.
+I made three questions, and set a time limit 100 seconds.
+If wrong answer is clicked, remain time will be decreased by 10 seconds.
 
-THEN a timer starts and I am presented with a question
-타이머가 시작되면서 질문이 나옴
+## Screenshot
+<img src="./assets/images/screenshot.png" />
 
-WHEN I answer a question
-대답을 선택하면 
-
-THEN I am presented with another question
-다음 질문이 나온다
-
-WHEN I answer a question incorrectly
-잘못 대답하면
-
-THEN time is subtracted from the clock
-시간 줄어듬
-
-WHEN all questions are answered or the timer reaches 0
-대답이 전부 끝나던지, 시간이 0이 되면
-
-THEN the game is over
-게임이 끝나고
-
-WHEN the game is over
-게임이 긑나면
-
-THEN I can save my initials and my score
-내 이니셜이랑 점수 저장 가능
+## Deployed application link
+https://d0kd0.github.io/hello_from_Korea
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,23 @@
-var question = document.getElementById('subject');
-var body = document.getElementById('description');
-var btn = document.querySelector('.btn');
+// variables clarification
+var question = document.getElementById('title');
+var body = document.getElementById('body');
+var btn = document.querySelector('.button');
 var score = 0;
 
 
-var timer = document.createElement('time');
+// timer clarification
+var timer = document.createElement('timer');
 var timeRemain = 100;
 
-var quizResult = document.createElement('div');
-
-
-
-function countdown() {
+function startTimer() {
     var timerInterval = setInterval(function() {
     timeRemain --;
-    timer.textContent = 'Time Left : ' + timeRemain;
+    timer.textContent = 'Time Left : ' + timeRemain + ' sec';
     document.body.appendChild(timer);
 
     if(timeRemain <= 0)  {
         timeRemain = 0;
-        timer.textContent = 'Time Left : ' + timeRemain;
+        timer.textContent = 'Time Left : ' + timeRemain + ' sec';
         clearInterval(timerInterval);
         inputName();
     } else if (score == 3) {
@@ -28,105 +26,71 @@ function countdown() {
 }, 1000);
 }
 
-function quizResult () {
-
-    quizResult.correct = 'correct';
-    quizResult.incorrect = 'wrong';
-}
-
+// list clarification
 var ol = document.createElement('ol');
+    var choice1 = document.createElement('li');
+    var choice2 = document.createElement('li');
+    var choice3 = document.createElement('li');
+    var choice4 = document.createElement('li');
 
-var choice1 = document.createElement('li');
-var choice2 = document.createElement('li');
-var choice3 = document.createElement('li');
-var choice4 = document.createElement('li');
 
-
-//quiz
+// quiz clarification
 btn.addEventListener ('click', function () {
-    countdown();
+    startTimer();
 
+    //quiz 1
     question.textContent = 'Where is capital of South Korea?';
-    body.textContent = '';
 
+    body.textContent = '';
         choice1.textContent = 'Seoul';
         choice2.textContent = 'Daegu';
         choice3.textContent = 'Busan';
         choice4.textContent = 'Pohang';
 
     document.body.children[1].appendChild(ol);
-    document.body.children[1].children[0].appendChild(choice1);
-    document.body.children[1].children[0].appendChild(choice2);
-    document.body.children[1].children[0].appendChild(choice3);
-    document.body.children[1].children[0].appendChild(choice4);
+        document.body.children[1].children[0].appendChild(choice1);
+        document.body.children[1].children[0].appendChild(choice2);
+        document.body.children[1].children[0].appendChild(choice3);
+        document.body.children[1].children[0].appendChild(choice4);
 
     btn.remove();   
+  
     var li = document.querySelectorAll('li');
 
     for(i=0; i<li.length; i++){
-        var eachli = li[i]
-            li[i].addEventListener('click', function (event) {
-                event.preventDefault();
-    
-                if(event.target.textContent = 'Seoul') {
-                    score ++;
-                    quizTwo();
-                } else {
-                    timeRemain -= 10;
-                }
-            })
+    var eachli = li[i]
+        li[i].addEventListener('click', function (event) {
+            event.preventDefault();
+
+            if(event.target.textContent == choice1.textContent) {
+                score ++;
+                secondquiz();
+            } else {
+                timeRemain -= 10;
+                
+            }
+
+        })
     };
+}) ;
 
 
-    // quiz2
-    function quizTwo() {
+    // quiz 2
+    function secondquiz(){
 
         question.textContent = 'What is national flower of South Korea?';
-        body.textContent = '';
 
+        body.textContent = '';
             choice1.textContent = 'Jangmi';
             choice2.textContent = 'Mugunghwa';
             choice3.textContent = 'Tulip';
             choice4.textContent = 'Lily';
 
         document.body.children[1].appendChild(ol);
-        document.body.children[1].children[0].appendChild(choice1);
-        document.body.children[1].children[0].appendChild(choice2);
-        document.body.children[1].children[0].appendChild(choice3);
-        document.body.children[1].children[0].appendChild(choice4);
-
-        var li = document.querySelectorAll('li');
-
-        for(i=0; i<li.length; i++){
-            var eachli = li[i]
-                li[i].addEventListener('click', function (event) {
-                    event.preventDefault();
-        
-                    if(event.target.textContent = 'Mugunghwa') {
-                        score ++;
-                        quizLast();
-                    } else {
-                        timeRemain -= 10;
-                    }
-                })
-        };
-    };
-    
-    // quiz3
-   function quizLast(){
-        question.textContent = 'What is the first flag of South Korea?';
-        body.textContent = '';
-
-        choice1.textContent = 'Sungjogi';
-        choice2.textContent = 'Mangukgi';
-        choice3.textContent = 'Taegeukgi';
-        choice4.textContent = 'Gwangbokgi';
-
-        document.body.children[1].appendChild(ol);
-        document.body.children[1].children[0].appendChild(choice1);
-        document.body.children[1].children[0].appendChild(choice2);
-        document.body.children[1].children[0].appendChild(choice3);
-        document.body.children[1].children[0].appendChild(choice4);
+            document.body.children[1].children[0].appendChild(choice1);
+            document.body.children[1].children[0].appendChild(choice2);
+            document.body.children[1].children[0].appendChild(choice3);
+            document.body.children[1].children[0].appendChild(choice4);
 
         var li = document.querySelectorAll('li');
 
@@ -134,26 +98,67 @@ btn.addEventListener ('click', function () {
         var eachli = li[i]
             li[i].addEventListener('click', function (event) {
                 event.preventDefault();
-                if((event.target.textContent = 'Taegeukgi') && (timeRemain>0)) {
+
+                if((event.target.textContent == choice2.textContent) && (timeRemain>0)) {
+                    score ++;
+                    lastquiz();
+                } else {
+                    timeRemain -= 10;
+                    console.log(timeRemain);
+                }
+                
+            });
+        }
+};
+    
+    
+
+    // quiz3
+    function lastquiz(){
+
+        question.textContent = 'Which King invented Hangul?';
+    
+        body.textContent = '';    
+            choice1.textContent = 'Youngjo';
+            choice2.textContent = 'Gwanghaegun';
+            choice3.textContent = 'Seongjong';
+            choice4.textContent = 'Sejong';
+    
+        document.body.children[1].appendChild(ol);
+            document.body.children[1].children[0].appendChild(choice1);
+            document.body.children[1].children[0].appendChild(choice2);
+            document.body.children[1].children[0].appendChild(choice3);
+            document.body.children[1].children[0].appendChild(choice4);
+    
+        var li = document.querySelectorAll('li');
+    
+        for(i=0; i<li.length; i++){
+        var eachli = li[i]
+            li[i].addEventListener('click', function (event) {
+                event.preventDefault();
+    
+                if((event.target.textContent == choice4.textContent) && (timeRemain>0)) {
                     score ++;
                     inputName();
+                } else if ((event.target.textContent != choice4.textContent) && (timeRemain>0)) {
+                    timeRemain -= 10;
                 }
-            });
+            })
         }
     };
 
+    // result submission
     var label = document.createElement('label');
     var input = document.createElement('input');
 
-
     function inputName (){
         question.textContent = 'All done!';
-        body.textContent = "Your final score is " + timeRemain +'.';
-
-        label.textContent = 'Enter initial: ';
-        input.setAttribute('width', '20%');
-        document.body.appendChild(label).appendChild(input);
+        body.textContent = "Your final score is " +timeRemain +' seond.';
         
+        label.textContent = 'Enter your initial: ';
+        input.setAttribute('width', '50%');
+        document.body.appendChild(label).appendChild(input);
+
         var submitbtn = document.createElement('button');
         submitbtn.textContent = "Submit";
         document.body.appendChild(submitbtn);
@@ -168,8 +173,8 @@ btn.addEventListener ('click', function () {
 
 
     function finalscore () {
-        var playerScore = localStorage.getItem("score");
-        var playerName = localStorage.getItem("name");
+        var myscore = localStorage.getItem("score");
+        var myname = localStorage.getItem("name");
 
         input.textContent = "";
         label.textContent = "";
@@ -177,31 +182,35 @@ btn.addEventListener ('click', function () {
 
         question.textContent = 'Highscores';
 
-
-        var scoreboard = document.createElement('li');
-        scoreboard.textContent = playerName + ' - ' + playerScore;
+        var scoreboard = document.createElement('score');
+        scoreboard.textContent = myname + ' - ' + myscore + ' second';
 
         document.body.append(scoreboard);
 
-
         var clearbtn = document.createElement('button');
-        var goBackbtn = document.createElement('button');
+        var gotobackbtn = document.createElement('button');
 
-        clearbtn.textContent = 'Clear score';
-        goBackbtn.textContent = 'Restart'
+        clearbtn.textContent = 'Clear';
+        gotobackbtn.textContent = 'Restart';
 
         document.body.appendChild(clearbtn);
-        document.body.appendChild(goBackbtn);
+        document.body.appendChild(gotobackbtn);
 
         clearbtn.addEventListener('click', function() {
             scoreboard.remove();
         });
 
-        goBackbtn.addEventListener('click', function(){
-            location.reload()}
+        gotobackbtn.addEventListener('click', function(){
+            location.reload()
+            resetStatus();}
             );
     }
 
+    function resetStatus() {
+        clearStatusClass(document.body);
+    }
 
-
-})
+    function clearStatusClass (element) {
+        element.classList.remove('correct');
+        element.classList.remove('wrong');
+    }
